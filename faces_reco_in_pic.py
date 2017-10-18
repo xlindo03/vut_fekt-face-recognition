d3bed4aca28f2f980b251e6dfdaf956ea71ec885
xlindo03 <32684133+xlindo03@users.noreply.github.com>
2017-10-18 21:11:25 +0300

    #

--- faces_reco_in_pic.py
+++ faces_reco_in_pic.py
@@ -3,10 +3,6 @@ from numpy.ma.core import array
 from scipy.misc.common import face
 import time
 from os import walk
-
-
-
-
 from funkce import *
 
 
@@ -102,37 +98,10 @@ for i, img in enumerate(filenames):
     print("Cas vypoctu: {} ms".format(round(duration*1000,2)))
 
 
-    print("Je to leos? {}".format(boolAnoNe(results[0])))
-    print("Je to jaromir? {}".format(boolAnoNe(results[1])))
-    print("Je to bohuslav? {}\n".format(boolAnoNe(results[2])))
-
-'''
-for i in range(1,6):
-    print("Obrazek cislo: ",i)
-
-
-
-    unknow_image = face_recognition.load_image_file("./unknow_faces/{}.jpg".format(i))
-
-    #face_recognition.face_locations(image,model="hog") hog => CPU | cnn => GPU
-    face_locations = face_recognition.face_locations(unknow_image) # vrati souradnice v poli [(top, left, bottom, right),(...)]
-
-    start_time = time.time()
-    unknow_face_encoding = face_recognition.face_encodings(unknow_image)[0]
-    results = face_recognition.compare_faces(know_faces, unknow_face_encoding)
-    stop_time=time.time()
-
-    duration=stop_time-start_time
-
-    print(int(round(duration*1000.0)),"ms")
-    print("Pocet obliceju: {}".format(len(face_locations)))
-    print("Souradnice obliceju: {}\n".format(face_locations))
-    print("Cas vypoctu: {} ms".format(int(round(duration*1000))))
 
     print("Je to leos? {}".format(boolAnoNe(results[0])))
     print("Je to jaromir? {}".format(boolAnoNe(results[1])))
     print("Je to bohuslav? {}\n".format(boolAnoNe(results[2])))
-'''
 
 
 
